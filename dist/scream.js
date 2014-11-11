81eaf3221d3fde3fe96198d64b64c1d8add3a2dd
Gajus Kuizinas <gk@anuary.com>
2014-11-11 17:26:14 +0200

    Added viewchange event.

--- dist/scream.js
+++ dist/scream.js
@@ -1,5 +1,5 @@
 /**
- * @version 2.0.2
+ * @version 2.0.3
  * @link https://github.com/gajus/scream for the canonical source repository
  * @license https://github.com/gajus/scream/blob/master/LICENSE BSD 3-Clause
  */
