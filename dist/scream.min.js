316704fad775b0f78dbe43aafd81a2a74112f70e
Gajus Kuizinas <gk@anuary.com>
2014-11-11 17:06:06 +0200

    Tested on all iOS devices, in a portrait and landscape mode.

--- dist/scream.min.js
+++ dist/scream.min.js
@@ -1,5 +1,5 @@
 /**
- * @version 2.0.1
+ * @version 2.0.2
  * @link https://github.com/gajus/scream for the canonical source repository
  * @license https://github.com/gajus/scream/blob/master/LICENSE BSD 3-Clause
  */
